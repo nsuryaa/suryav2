925b646982ad9dd52d4ba1932bca60fa40ae24ee
nsuryaa <suryasedulous@gmail.com>
2024-02-22 12:35:18 +0530

    initial commit

---
+++ src/App/config.tsx
@@ -0,0 +1,41 @@
+import { Config } from 'types';
+import { Email, GitHub, LinkedIn, Resume } from 'icons';
+
+export const config: Config = {
+  name: {
+    display: 'Surya',
+  },
+  title: {
+    display: 'Aspiring Software Engineer',
+  },
+  buttons: [
+    {
+      name: 'github',
+      display: 'GitHub',
+      ariaLabel: 'GitHub profile (opens in new window)',
+      icon: <GitHub />,
+      href: 'https://github.com/nsuryaa/',
+    },
+    {
+      name: 'linked-in',
+      display: 'LinkedIn',
+      ariaLabel: 'LinkedIn profile (opens in new window)',
+      icon: <LinkedIn />,
+      href: 'https://www.linkedin.com/in/nsuryaa/',
+    },
+    {
+      name: 'resume',
+      display: 'Resume',
+      ariaLabel: 'Resume in Google Drive (opens in new window)',
+      icon: <Resume />,
+      href: '',
+    },
+    {
+      name: 'email',
+      display: 'Email',
+      ariaLabel: 'Email contact (opens in new window)',
+      icon: <Email />,
+      href: 'mailto:surya221211@gmail.com',
+    },
+  ],
+};
